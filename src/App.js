dbbea348c069e3056b20fcee7c0c5f0c44e35dbe
Seeun Bae <morendo90@gmail.com>
2020-10-01 11:25:06 +0900

    Load movie data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
@@ -6,12 +7,19 @@ class App extends React.Component {
     movies: [],
   }
 
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    // movies: movies => ES6에서는 객체의 키와 대입할 변수의 이름이 같다면 코드를 축약할 수 있다.
+    this.setState({ movies, isLoading: false })
+  }
+
   componentDidMount() {
     // 영화 데이터 로딩!
-
-    setTimeout(() => {
-      this.setState({ isLoading: false});
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
